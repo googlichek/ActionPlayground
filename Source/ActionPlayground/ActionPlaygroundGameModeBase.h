630745b2714fdc34b0dfe0441a003b7f581d57b4
Goga Akobiya <googlichek.one@gmail.com>
2022-07-20 15:15:53 +0300

    Project setup.

---
+++ Source/ActionPlayground/ActionPlaygroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionPlaygroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONPLAYGROUND_API AActionPlaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
